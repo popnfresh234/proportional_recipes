b857442367d6b5f78ca0060d8c82d6de23b4ba2a
lenovo <ajhollid@gmail.com>
2016-03-31 19:17:44 +0800

    feat: uploading validates with data from server for adding new recipes

--- app/src/main/java/alexander/dmtaiwan/com/proportionalrecipes/Models/MainInteractorImpl.java
+++ app/src/main/java/alexander/dmtaiwan/com/proportionalrecipes/Models/MainInteractorImpl.java
@@ -17,7 +17,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 /**
  * Created by lenovo on 3/25/2016.
  */
-public class MainInteractorImpl implements MainInteractor{
+public class MainInteractorImpl implements MainInteractor {
 
     private MainInteractorListener listener;
     private Context context;
@@ -29,36 +29,19 @@ public class MainInteractorImpl implements MainInteractor{
 
     @Override
     public void fetchData() {
-//        Dummy Data
-//        ArrayList<Recipe> downloadRecipes = new ArrayList<Recipe>();
-//        for (int i = 0; i < 5; i++) {
-//            String name = "Recipe " + String.valueOf(i);
-//            ArrayList<Ingredient> ingredientList = new ArrayList<>();
-//            for (int j = 1; j < 10; j++) {
-//                String ingredientName = "Ingredient " + String.valueOf(j);
-//                Ingredient ingredient = new Ingredient(ingredientName, j + 10);
-//                ingredientList.add(ingredient);
-//            }
-//            Recipe recipe = new Recipe(name, ingredientList);
-//            downloadRecipes.add(recipe);
-//
-//        }
-//        Log.i("TEST", String.valueOf(downloadRecipes.size()));
-//                listener.onResult(downloadRecipes);
 
         if (Utilities.doesFileExist(context)) {
             String jsonRecipeList = Utilities.readFromFile(context);
             ArrayList<Recipe> recipeList = Utilities.recipesFromJson(jsonRecipeList);
             listener.onResult(recipeList);
-        }
-        else {
+        } else {
             listener.onResult(new ArrayList<Recipe>());
         }
 
     }
 
     @Override
-    public void uploadData(ArrayList<Recipe> recipeList, final Context context) {
+    public void uploadData(final ArrayList<Recipe> recipeList, final Context context) {
         listener.showLoading();
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(Utilities.BASE_URL)
@@ -66,20 +49,66 @@ public class MainInteractorImpl implements MainInteractor{
                 .build();
 
         RecipeService service = retrofit.create(RecipeService.class);
-        Call<ArrayList<Recipe>> upload = service.uploadRecipes(Utilities.JSON_ID, recipeList);
+        Call<ArrayList<Recipe>> download = service.downloadRecipes(Utilities.JSON_ID);
+        download.enqueue(new Callback<ArrayList<Recipe>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Recipe>> call, retrofit2.Response<ArrayList<Recipe>> response) {
+                validateData(recipeList, response.body());
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Recipe>> call, Throwable t) {
+                listener.makeSnackBar(context.getString(R.string.download_failure));
+            }
+        });
+    }
+
+    private void validateData(ArrayList<Recipe> localList, ArrayList<Recipe> remoteList) {
+        final ArrayList<Recipe> newRemoteList = new ArrayList<>();
+        //Populate new recipe list for upload
+        for (Recipe recipe : remoteList) {
+            newRemoteList.add(recipe);
+        }
+
+        //Loop through localList and look for new recipes, add if new
+        for (int i = 0; i < localList.size(); i++) {
+            Recipe localRecipe = localList.get(i);
+            boolean contained = false;
+            for (int j = 0; j < remoteList.size(); j++) {
+                Recipe remoteRecipe = remoteList.get(j);
+                if (localRecipe.getId() == remoteRecipe.getId()) {
+                    contained = true;
+                }
+            }
+            if (!contained) {
+                newRemoteList.add(localRecipe);
+            }
+        }
+
+        //Upload Data
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Utilities.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RecipeService service = retrofit.create(RecipeService.class);
+        Call<ArrayList<Recipe>> upload = service.uploadRecipes(Utilities.JSON_ID, newRemoteList);
         upload.enqueue(new Callback<ArrayList<Recipe>>() {
             @Override
             public void onResponse(Call<ArrayList<Recipe>> call, retrofit2.Response<ArrayList<Recipe>> response) {
                 listener.hideLoading();
                 listener.makeSnackBar(context.getString(R.string.upload_success));
+                listener.onResult(newRemoteList);
             }
 
             @Override
             public void onFailure(Call<ArrayList<Recipe>> call, Throwable t) {
                 listener.hideLoading();
                 listener.makeSnackBar(context.getString(R.string.upload_failure));
+                listener.onResult(newRemoteList);
             }
         });
+
     }
 
     @Override
@@ -100,7 +129,7 @@ public class MainInteractorImpl implements MainInteractor{
 
                 ArrayList<Recipe> recipeList = response.body();
                 String json = new Gson().toJson(recipeList);
-                Utilities.writeToFile(json,context);
+                Utilities.writeToFile(json, context);
                 listener.onResult(recipeList);
 
             }
